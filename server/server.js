56db44110e13fc1190ddc2c529dacddc4f56b635
mcdanije22 <mcdanije22@gmail.com>
2019-04-08 16:43:19 -0400

    fix files

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, ()=>console.log('test'));
